d2fef3948f490707a59de8d74c730bd23482ff7d
Rezaqul Khaq <rqk.email2@gmail.com>
2022-02-27 15:08:07 +1100

    edit models in kontrak app, add VolAndTime table

--- kontrak/admin.py
+++ kontrak/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Client, Project, CostAcc, Unit, WorkItem, Rab
+from .models import Client, Project, CostAcc, Unit, WorkItem, Rab, VolAndTime
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Project)
@@ -7,4 +7,5 @@ admin.site.register(CostAcc)
 admin.site.register(Unit)
 admin.site.register(WorkItem)
 admin.site.register(Rab)
+admin.site.register(VolAndTime)
 
--- kontrak/models.py
+++ kontrak/models.py
@@ -9,15 +9,18 @@ class Client(models.Model):
   address = models.CharField(max_length=255)
   phone = models.CharField(max_length=20, blank=True)
   email = models.EmailField(blank=True)
+  created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
   def __str__(self):
     return self.name
 
+
 class Project(models.Model):
-  created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
   name = models.CharField('Nama Proyek',max_length=255, db_index=True)
-  client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)
+  client = models.ForeignKey(Client, on_delete=models.RESTRICT, null=True)
   project_address = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
   start_date = models.DateTimeField(blank=True)
   description = models.TextField(blank=True)
 
@@ -26,7 +29,7 @@ class Project(models.Model):
 
 class CostAcc(models.Model):
   name = models.CharField('Pos Biaya',max_length=50, db_index=True)
-  description = models.CharField(max_length=255)
+  description = models.TextField(blank=True)
 
   def __str__(self):
     return self.name
@@ -41,25 +44,32 @@ class Unit(models.Model):
 
 class WorkItem(models.Model):
   name = models.CharField('Nama Pekerjaan',max_length=255, db_index=True)
-  cost_acc = models.ForeignKey(CostAcc, on_delete=models.DO_NOTHING, null=True)
-  unit = models.ForeignKey(Unit, on_delete=models.DO_NOTHING, null=True)
+  city = models.CharField(max_length=255, blank=True)
+  cost_acc = models.ForeignKey(CostAcc, on_delete=models.RESTRICT, default=1)
+  unit = models.ForeignKey(Unit, on_delete=models.RESTRICT, default=1)
   unit_price = models.DecimalField(max_digits=20, decimal_places=0)
   unit_budget= models.DecimalField(max_digits=20, decimal_places=0)
-  city = models.CharField(max_length=255, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
-    return self.name
+    return self.name + ' ' + self.cost_acc.name + ' ' + self.unit.name
 
 class Rab(models.Model):
   project = models.OneToOneField(Project, on_delete=models.CASCADE)
-  work_item = models.ForeignKey(WorkItem, on_delete=models.DO_NOTHING, null=True)
-  unit = models.ForeignKey(Unit, on_delete=models.DO_NOTHING, null=True)
+  name = models.CharField('Nama RAB', max_length=255, default='RAB Total')
+  phase = models.CharField(max_length=255, blank=True)
+
+  def __str__ (self):
+    return self.project.name + ' ' + self.name + ' ' + self.phase
+
+class VolAndTime(models.Model):
+  rab = models.ForeignKey(Rab, on_delete=models.CASCADE)
+  work_item = models.ForeignKey(WorkItem, on_delete=models.RESTRICT)
   volume = models.DecimalField(max_digits=10, decimal_places=2)
   start_date = models.DateTimeField(blank=True, null=True)
   duration = models.IntegerField(blank=True, null=True)
 
-  def __str__(self):
-    return self.project.name + ' ' + self.work_item.name
+  def __str__ (self):
+    return self.rab.name + ' ' + self.work_item.name
