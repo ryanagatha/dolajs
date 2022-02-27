d62180762b8832ae40e44c6c059b393fe4253c12
Rezaqul Khaq <rqk.email2@gmail.com>
2022-02-28 00:23:17 +1100

    add ERM link on readme file

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Backend Dola - Aplikasi manajemen untuk UMKM berbasis proyek
 
 build with Django
+
+dabase ERD can be found here : https://dbdiagram.io/d/621ad5e854f9ad109a283a50
--- kontrak/admin.py
+++ kontrak/admin.py
@@ -1,3 +1,13 @@
+import django
 from django.contrib import admin
+import django.apps
 # Register your models here.
+models = django.apps.apps.get_models()
+
+for model in models:
+  try:
+    admin.site.register(model)
+  except admin.sites.AlreadyRegistered:
+    pass
+    
 
