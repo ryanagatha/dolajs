79c446bae9597d7e08d411680bb3f62e0352b890
USER <user@192.168.1.9>
2022-03-01 18:08:18 +0700

    dola lahirv2 tambah secret key

--- dola/settings.py
+++ dola/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY ='hj=qy=3wb7+jfo5chek2gqye#deyl&9^o+@r94^9!b((mt$al4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
